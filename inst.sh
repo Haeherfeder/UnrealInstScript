a2dcfbd8d04a1d5908822e735a04534849fe392c
Haeherfeder <github@haeherfeder.de>
2019-02-24 21:28:38 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 git clone https://github.com/EpicGames/UnrealEngine.git
+wget us.download.nvidia.com/XFree86/Linux-x86_64/418.43/NVIDIA-Linux-x86_64-418.43.run
+chmod +x NVIDIA-Linux-x86_64-418.43.run
+./NVIDIA-Linux-x86_64-418.43.run
 cd UnrealEngine
 ./Setup.sh
 ./GenerateProjectFiles.sh 
